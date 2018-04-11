048272dece7928670f349f10034efd8f4e699567
254332471@qq.com <254332471@qq.com>
2018-04-11 22:17:16 +0800

    add

---
+++ test/paimai/pages/pay/pay.js
@@ -0,0 +1,29 @@
+// pages/pay/pay.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    bigPic:'../../images/pic.png',
+    smallPic: '../../images/pic.png',
+    startPrice: 99,
+    endPrice: 499,
+    time:1,
+    nowPrice:0
+  },
+
+  onLoad: function (options) {
+    this.setData({
+      startPrice: this.data.startPrice.toFixed(2),
+      endPrice: this.data.endPrice.toFixed(2),
+      nowPrice: parseInt(this.data.startPrice),
+    })
+  },
+  sliderchange(e) {
+    this.setData({
+      nowPrice: e.detail.value,
+    })
+    console.log(e.detail.value)
+  }
+})
